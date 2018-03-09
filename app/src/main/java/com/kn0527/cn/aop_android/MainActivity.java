4aa71d8ee399b9ddab4b7342220d53a6a53c7332
01-00240-PC\01-00240 <554193697@qq.com>
2018-03-09 17:47:32 +0800

    布局提交

--- app/src/main/java/com/kn0527/cn/aop_android/MainActivity.java
+++ app/src/main/java/com/kn0527/cn/aop_android/MainActivity.java
@@ -1,7 +1,11 @@
 package com.kn0527.cn.aop_android;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +14,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void mShake(View view) {
+        SystemClock.sleep(new Random().nextInt(2000));
+        long begin = System.currentTimeMillis();
+        SystemClock.sleep(new Random().nextInt(2000));
+        long duration = System.currentTimeMillis() - begin;
+        System.out.println("摇一摇功能花费了 "+duration+"毫秒");
+    }
+
+    public void mAudio(View view) {
+
+    }
+
+    public void mVideo(View view) {
+
+    }
+
+    public void saySomething(View view) {
+
+    }
+
 }
